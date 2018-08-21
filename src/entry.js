79dc3df561e04c1927bb1b9e61475c05835cecbc
Anton Barua <anton.085@gmail.com>
2018-08-20 22:15:46 -0500

    array and eslint changes

--- src/entry.js
+++ src/entry.js
@@ -1,4 +1,15 @@
-import {vardecl} from './vardecl';
+import vardecl from './vardecl';
+import * as arrFunctions from './array';
+
 console.log('welcome');
 
 vardecl();
+
+arrFunctions.printArr();
+arrFunctions.forEach();
+arrFunctions.map();
+arrFunctions.filter();
+arrFunctions.find();
+arrFunctions.every();
+arrFunctions.some();
+arrFunctions.reduce();
--- src/vardecl.js
+++ src/vardecl.js
@@ -1,52 +1,50 @@
-function vardecl() {
-    console.log('---variable declaration---');
+export default function vardecl() {
+  console.log('---variable declaration---');
 
-    console.log('let a, var b, const c');
-    console.log(`b outside block before decl:${b}`);
+  console.log('let a, var b, const c');
+  console.log(`b outside block before decl:${b}`);
 
 
-    let a = 1;
-    var b = 2;
-    const c = 3;
+  const a = 1;
+  var b = 2;
+  const c = 3;
 
-    console.log(`a outside before block:${a}`);
-    console.log(`b outside before block:${b}`);
-    console.log(`c outside before block:${c}`);
+  console.log(`a outside before block:${a}`);
+  console.log(`b outside before block:${b}`);
+  console.log(`c outside before block:${c}`);
 
-    {
-        try {
-            console.log(`a inside block before redecl:`);
-            console.log(`a inside block before redecl:${a}`);
-        } catch (e) {
-            console.log(e);
-        }
-        console.log(`b inside block before redecl:${b}`);
-        try {
-            console.log(`c inside block before redecl:`);
-            console.log(`c inside block before redecl:${c}`);
-        } catch (e) {
-            console.log(e);
-        }
-
-        let a = 100;
-        var b = 200;
-        const c = 300;
-
-        console.log(`a inside block after redecl:${a}`);
-        console.log(`b inside block after redecl:${b}`);
-        console.log(`c inside block after redecl:${c}`);
-
-        try {
-            console.log('try to redefine c:')
-            c = 500;
-        } catch (e) {
-            console.log(e);
-        }
+  {
+    try {
+      console.log('a inside block before redecl:');
+      console.log(`a inside block before redecl:${a}`);
+    } catch (e) {
+      console.error(e);
+    }
+    console.log(`b inside block before redecl:${b}`);
+    try {
+      console.log('c inside block before redecl:');
+      console.log(`c inside block before redecl:${c}`);
+    } catch (e) {
+      console.error(e);
     }
 
-    console.log(`a outside after block:${a}`);
-    console.log(`b outside after block:${b}`);
-    console.log(`c outside after block:${c}`);
-}
+    let a = 100;
+    var b = 200;
+    const c = 300;
 
-export { vardecl };
+    console.log(`a inside block after redecl:${a}`);
+    console.log(`b inside block after redecl:${b}`);
+    console.log(`c inside block after redecl:${c}`);
+
+    try {
+      console.log('try to redefine c:');
+      c = 500;
+    } catch (e) {
+      console.error(e);
+    }
+  }
+
+  console.log(`a outside after block:${a}`);
+  console.log(`b outside after block:${b}`);
+  console.log(`c outside after block:${c}`);
+}
